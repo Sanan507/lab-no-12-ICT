ec4783482738252ad218d199af8cffd3838a6d12
Muhammad Sanan Sarwar <sanansarwar507@gmail.com>
2025-12-05 11:36:06 +0500

    Add three Python function files

---
+++ add_numbers.py
@@ -0,0 +1,7 @@
+def add_numbers(a, b):
+    return a + b
+
+a = 5
+b = 15
+result = add_numbers(a, b)
+print("Sum of two numbers is:", result)
---
+++ multiply.py
@@ -0,0 +1,7 @@
+def multiply(a,b):
+   return a*b
+
+a=5
+b=8
+result=multiply(a,b)
+print("Product of numbers is:",result)
